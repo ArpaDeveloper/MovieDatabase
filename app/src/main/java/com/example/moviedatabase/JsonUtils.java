170118c8b6c1177767ea725714111096fbd7616d
Arpa <aarni.viljanen@gmail.com>
2025-03-19 16:00:25 +0200

    First commit

--- app/src/main/java/com/example/moviedatabase/JsonUtils.java
+++ app/src/main/java/com/example/moviedatabase/JsonUtils.java
@@ -2,16 +2,23 @@ package com.example.moviedatabase;
 
 import android.content.Context;
 
+
+import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.IOException;
-import java.lang.reflect.Type;
 import java.util.List;
+import java.util.ArrayList;
 
 public class JsonUtils {
     private static final String TAG = "JsonUtils";
 
-    public static List<Movie> loadMoviesFromJson(Context context, int resourceID) throws IOException{
-        InputStreamReader reader = new InputStreamReader(context.getAssets().open("movies.json"));
+    public static List<Movie> loadMoviesFromJson(Context context){
+        List<Movie> movieList = new ArrayList<>();
+
+        try {
+            //Open the movies.json from res/raw
+            InputStream inputStream = context.getResources().openRawResource(R.raw.movies);
+            InputStreamReader reader = new InputStreamReader(inputStream);
+        }
     }
 
     private static void handleJsonException(Exception e){
