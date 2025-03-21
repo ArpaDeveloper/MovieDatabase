9eab9c43bd0abea369cf9e49b421a66a87cea63f
Arpa <aarni.viljanen@gmail.com>
2025-03-21 10:52:58 +0200

    Final

--- app/src/main/java/com/example/moviedatabase/MainActivity.java
+++ app/src/main/java/com/example/moviedatabase/MainActivity.java
@@ -14,7 +14,6 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import org.json.JSONException;
-
 import java.io.IOException;
 import java.util.List;
 
